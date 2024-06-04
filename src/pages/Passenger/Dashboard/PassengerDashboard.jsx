ef05d720a6abd63cd6debf4894d1147689887a0f
magnuswretblad <mwdev81@gmail.com>
2024-06-04 20:10:19 +0200

    Refactor CSS styles for BookingCard and PassengerDashboard components

--- src/pages/Passenger/Dashboard/PassengerDashboard.jsx
+++ src/pages/Passenger/Dashboard/PassengerDashboard.jsx
@@ -11,7 +11,6 @@ const PassengerDashboard = () => {
   const userPhone = user.user.phone;
   const userPassword = user.user.password;
 
-  // console.log(bookings);
   useEffect(() => {
     const fetchBookings = async () => {
       const user = JSON.parse(localStorage.getItem("user-passenger"));
@@ -30,23 +29,50 @@ const PassengerDashboard = () => {
   }, []);
 
   return (
-    <>
-      <div>
-        <p>alla mina bokningar:</p>
-        {/* render the bookings here */}
+    <div className="passenger-dashboard-container">
+      <div className="passenger-dashboard-wrapper">
+        <div className="passenger-dashboard-account-container">
+          <h2>Mina kontouppgifter:</h2>
+          <div className="passenger-dashboard-account-group">
+            <p>Email: </p>
+            <p>{userEmail}</p>
+          </div>
+          <div className="passenger-dashboard-account-group">
+            <p>Telefonnummer: </p>
+            <p>{userPhone}</p>
+          </div>
+
+          <div>
+            <p>Ändra lösenord:</p>
+            <div className="passenger-dashboard-change-password">
+              <input
+                className="passenger-dashboard-change-password-input"
+                type="password"
+                placeholder="Ange nuvarande lösenord"
+              />
+              <input
+                className="passenger-dashboard-change-password-input"
+                type="password"
+                placeholder="Ange nytt lösenord"
+              />
+            </div>
+          </div>
+          <div className="passenger-dashboard-btns">
+            <button className="passenger-dashboard-change-password-btn">
+              Uppdatera lösenord
+            </button>
+            <button className="passenger-dashboard-delete-account-btn">
+              Radera mitt konto
+            </button>
+          </div>
+        </div>
+
+        <h2>Alla mina bokningar:</h2>
         {bookings.map((booking, index) => (
           <BookingCard key={index} booking={booking} />
         ))}
       </div>
-      <div>
-        <p>Mina kontouppgifter:</p>
-        <div>
-          <p>Email: {userEmail}</p>
-          <p>Telefonnummer: {userPhone}</p>
-          <p>Ändra lösenord:</p>
-        </div>
-      </div>
-    </>
+    </div>
   );
 };
 
