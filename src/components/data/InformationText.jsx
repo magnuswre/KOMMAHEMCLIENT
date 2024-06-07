752596d00b16037c74b437ae5e8b9e54837ff33d
magnuswretblad <mwdev81@gmail.com>
2024-06-07 14:58:50 +0200

    chore: Refactor InformationText component for clarity and consistency

--- src/components/data/InformationText.jsx
+++ src/components/data/InformationText.jsx
@@ -6,24 +6,24 @@ const InformationText = () => {
     <div className="InformationText-container">
       <h2>Så här fungerar Kommahem:</h2>
       <p>
-        För resernärer, med eller utan bil, som i samband med färjorna som
+        För resenärer, med eller utan bil, som i samband med färjorna som
         anländer till Visby hamn vill kunna samåka hela eller delar av vägen.
-        Slutdestination kan endast anges med sockennamn eller tätortsnamn.
+        Slutdestinationen kan endast anges med sockennamn eller tätortsnamn
       </p>
-      <h2>Passagerare som har bil:</h2>
+      <h2>Passagerare med bil:</h2>
       <p>
-        Lägger upp en körning och anger: datum, slutdestination, rutt och antal
-        platser.
+        Lägger upp en körning och anger: datum, slutdestination, båttur och
+        antal platser
       </p>
       <h2>Passagerare utan bil:</h2>
       <p>
-        Har möjlighet att söka på datum, slutdestination, rutt och antal
-        platser.
+        Har möjlighet att söka på datum, slutdestination, båttur och antal
+        platser
       </p>
       <h2>Samåkning:</h2>
       <p>
-        Den bilburna och med passageraren/na sköter all kontakt internt, via
-        mejl eller telefon
+        Förare och passagerare sköter själva kontakten via t.ex. mejl eller
+        telefon
       </p>
     </div>
   );
