640e005a408e6025b39b1cab4c434a6c747facad
magnuswretblad <mwdev81@gmail.com>
2024-06-09 10:51:56 +0200

    Update base URL to use production API endpoint

--- src/components/DriverForm/DriverForm.jsx
+++ src/components/DriverForm/DriverForm.jsx
@@ -3,7 +3,7 @@ import "./DriverForm.css";
 import { useNavigate } from "react-router-dom";
 import { DriverContext } from "../../contexts/DriverContext";
 import MyDatePickerComponent from "../DatePickerComponents/DatePickerComponentDriver/MyDatePickerComponent";
-const baseUrl = import.meta.env.VITE_APP_BASE_URL;
+const baseUrl = "https://kommahem-fd9ac0fc3b1a.herokuapp.com";
 
 const DriverForm = () => {
   const {
--- src/pages/Passenger/PassengerReset/PassengerReset.jsx
+++ src/pages/Passenger/PassengerReset/PassengerReset.jsx
@@ -3,7 +3,7 @@ import React, { useContext, useEffect, useState } from "react";
 import "./PassengerReset.css";
 import { useNavigate } from "react-router-dom";
 import { RecoveryContext } from "../../../contexts/RecoveryContext";
-const baseUrl = import.meta.env.VITE_APP_BASE_URL;
+const baseUrl = "https://kommahem-fd9ac0fc3b1a.herokuapp.com";
 
 const PassengerReset = () => {
   const { userId } = useContext(RecoveryContext);
