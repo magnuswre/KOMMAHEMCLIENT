e9f36030ffd65a2d5e136596a9ed3be29018508e
magnuswretblad <mwdev81@gmail.com>
2024-06-11 16:08:22 +0200

    style: Update CSS for better styling and consistency

--- src/pages/Passenger/PassengerConfirm/PassengerConfirm.jsx
+++ src/pages/Passenger/PassengerConfirm/PassengerConfirm.jsx
@@ -21,27 +21,8 @@ const PassengerConfirm = () => {
   return (
     <div className="Passenger-confirm-container">
       <div className="Passenger-confirm-wrapper">
-        <div>
-          <button
-            type="button"
-            onClick={handleSubmit}
-            className="passenger-confirm-page-btn"
-          >
-            Logga ut
-          </button>
-        </div>
-        <div>
-          <p>Bekräftelse skickas även till din angivna mejladress</p>
-        </div>
-        <div>
-          <button
-            type="button"
-            onClick={handleChange}
-            className="passenger-confirm-page-btn"
-          >
-            Se alla mina bokningar och konto
-          </button>
-        </div>
+        <h2>Tack för din bokning!</h2>
+        <p>Bekräftelse skickas även till din angivna mejladress</p>
       </div>
     </div>
   );
