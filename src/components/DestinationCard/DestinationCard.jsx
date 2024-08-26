f6ed421e6625c9c0d7534c62ecab88fd8215e6af
magnuswretblad <mwdev81@gmail.com>
2024-08-26 09:12:37 +0200

    increase size of card close button

--- src/components/DestinationCard/DestinationCard.jsx
+++ src/components/DestinationCard/DestinationCard.jsx
@@ -1,11 +1,26 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import "./DestinationCard.css";
 import { DriverContext } from "../../contexts/DriverContext";
 import BookingsOfDestinationCard from "../BookingsDestinationCard/BookingsOfDestinationCard";
 
-const DestinationCard = ({ destination, bookingsDestinations }) => {
+const DestinationCard = ({ destination }) => {
   const { getBookingsForDestination } = useContext(DriverContext);
   const [bookings, setBookings] = useState([]);
+
+  useEffect(() => {
+    const fetchInitialBookings = async () => {
+      try {
+        const initialBookings = await getBookingsForDestination(
+          destination.DestinationId
+        );
+        setBookings(initialBookings);
+      } catch (error) {
+        console.error("Error:", error.message);
+      }
+    };
+
+    fetchInitialBookings();
+  }, []);
 
   const handleClick = async () => {
     try {
@@ -17,25 +32,21 @@ const DestinationCard = ({ destination, bookingsDestinations }) => {
       console.error("Error:", error.message);
     }
   };
-  const formatTime = (time) => {
-    if (time) {
-      return time.slice(0, -3);
-    }
-    return time;
-  };
 
-  console.log(destination);
+  const formatTime = (time) => {
+    return time ? time.slice(0, -3) : time;
+  };
 
   return (
     <div className="destination-card-container" onClick={handleClick}>
       <div className="destination-card-notice">
-        <p>1</p>
+        <p>{bookings.length > 0 ? "!" : "-"}</p>
       </div>
       <p>Datum: {destination.DestinationTravelDate}</p>
       <p>Destination: {destination.EndDestination}</p>
       <p>Bookingsid: {destination.DestinationId}</p>
       <p>
-        Platser kvar: {destination.DestinationSeats} av <span> </span>
+        Platser kvar: {destination.DestinationSeats} av{" "}
         {destination.OriginalSeats}
       </p>
       <p>Båttur: {destination.Route}</p>
